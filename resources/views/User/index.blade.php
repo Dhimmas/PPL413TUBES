9b4c2a69ca14ee02e92fe1ccad0f6c214ab26737
BadakMancung <ifan04ashar@gmail.com>
2025-06-11 22:47:19 +0700

    finalisasi

--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -1,4 +1,6 @@
-<x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -273,4 +275,5 @@
     </div>
   </div>
 </body>
-</x-app-layout>
+
+</html>
